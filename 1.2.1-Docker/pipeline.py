a9fab1163ddf097dad1654d422f6c4ad56e3b70d
jihwan <cuchoco@naver.com>
2022-06-12 21:48:40 +0900

    second

--- 1.2.1-Docker/pipeline.py
+++ 1.2.1-Docker/pipeline.py
@@ -1,5 +1,12 @@
+import sys
 import pandas as pd
+
+print(sys.argv)
+
+day = sys.argv[1]
+
 
 # some fancy stuff with pandas
 
-print('job finished succeessfully')
+
+print(f'job finished succeessfully for day = {day}')
