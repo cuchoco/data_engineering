9f848769c20bf996420801b17e9ddf1c175d6be6
jihwan <cuchoco@naver.com>
2022-06-16 10:31:00 +0900

    folder changed

--- week1/parquet_to_csv.py
+++
@@ -1,6 +0,0 @@
-import pyarrow.parquet as pq
-trips = pq.read_table("/home/cuchoco/data_engineering/week1/yellow_tripdata_2022-01.parquet")
-trips = trips.to_pandas()
-
-print(type(trips))
-trips.to_csv('/home/cuchoco/data_engineering/week1/trip.csv', index=False)
