4033d40d2e4a03f37ac30f954551ce49436b78b7
jihwan <cuchoco@naver.com>
2022-06-18 00:37:19 +0900

    user change

--- 1.1_Docker_postgres/parquet_to_csv.py
+++ 1.1_Docker_postgres/parquet_to_csv.py
@@ -1,6 +1,6 @@
 import pyarrow.parquet as pq
-trips = pq.read_table("/home/cuchoco/data_engineering/1.1_Docker_postgres/yellow_tripdata_2022-01.parquet")
+trips = pq.read_table("/home/kuchoco/data_engineering/1.1_Docker_postgres/yellow_tripdata_2022-01.parquet")
 trips = trips.to_pandas()
 
 print(type(trips))
-trips.to_csv('/home/cuchoco/data_engineering/1.1_Docker_postgres/trip.csv', index=False)
+trips.to_csv('/home/kuchoco/data_engineering/1.1_Docker_postgres/trip.csv', index=False)
