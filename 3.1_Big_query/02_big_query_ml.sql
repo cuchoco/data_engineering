ca2efb71c93a9da2914e66ab282237deb64825b9
jihwan <cuchoco@naver.com>
2022-07-07 00:40:19 +0900

    ML

--- 3.1_Big_query/02_big_query_ml.sql
+++ 3.1_Big_query/02_big_query_ml.sql
@@ -1,4 +1,4 @@
--- SELECT THE SOLUMS INTERESTED FOR YOU
+-- SELECT THE COLUMS INTERESTED FOR YOU
 select passenger_count, trip_distance, PULocationID, DOLocationID, payment_type, fare_amount, tolls_amount, tip_amount
 from `indigo-anchor-353504.nytaxi.yellow_tripdata_partitioned` where fare_amount != 0;
 
@@ -26,3 +26,44 @@ OPTIONS
   DATA_SPLIT_METHOD='AUTO_SPLIT') AS
 SELECT * FROM `indigo-anchor-353504.nytaxi.yellow_tripdata_ml`
 WHERE tip_amount IS NOT NULL;
+
+
+-- check features
+select * from ML.FEATURE_INFO(MODEL `indigo-anchor-353504.nytaxi.tip_model`);
+
+
+-- EVALUATE THE MODEL
+SELECT * FROM
+ML.EVALUATE(MODEL `indigo-anchor-353504.nytaxi.tip_model`,
+(SELECT * FROM `indigo-anchor-353504.nytaxi.yellow_tripdata_ml`
+WHERE tip_amount IS NOT NULL));
+
+
+-- PREDICT THE MODEL
+SELECT * FROM
+ML.PREDICT(MODEL `indigo-anchor-353504.nytaxi.tip_model`,
+( SELECT * FROM `indigo-anchor-353504.nytaxi.yellow_tripdata_ml`
+WHERE tip_amount IS NOT NULL ));
+
+-- PREDICT AND EXPLAIN
+SELECT * FROM
+ML.EXPLAIN_PREDICT(MODEL `indigo-anchor-353504.nytaxi.tip_model`,
+(SELECT * FROM `indigo-anchor-353504.nytaxi.yellow_tripdata_ml`
+WHERE tip_amount IS NOT NULL), STRUCT(3 as top_k_features));
+
+
+-- HYPER PARAM TUNNING
+CREATE OR REPLACE MODEL `indigo-anchor-353504.nytaxi.tip_hyperparam_model`
+OPTIONS
+(model_type='linear_reg',
+input_label_cols=['tip_amount'],
+DATA_SPLIT_METHOD='AUTO_SPLIT',
+num_trials=5,
+max_parallel_trials=2,
+l1_reg=hparam_range(0, 20),
+l2_reg=hparam_candidates([0, 0.1, 1, 10])) AS
+SELECT * FROM `indigo-anchor-353504.nytaxi.yellow_tripdata_ml`
+WHERE tip_amount IS NOT NULL;
+
+
+
